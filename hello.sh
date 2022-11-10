d7761bfc638a629357e679c65311789bba092567
inadez <inadesiree1@hotmail.com>
2022-11-10 15:26:05 -0600

    new info added to files or directories

--- hello.sh
+++ hello.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
     #Author : Utrains
-    #Date : 01-Nov-2021
+    #Date created : 01-Nov-2021
+    #Date modified: 06-NOV-2022
+## ---------- script that display hello world in the screen -----------------
 
-## ---------- script that display hello word in the screen -----------------
+echo "Hello World !"
+#sleep 3
+echo "Hello Desiree! How are you today?"
+#sleep 3
+echo "What can I do for you today?"
+#sleep 3
+pwd
+#sleep 3
+echo "Your kernel version is"
+uname -r
 
-echo "Yello Word !"
